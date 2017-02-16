cd1c19cc5cb0c720ae95a79d365fac34134ad1bd
gubin <gubin@192.168.0.132>
2017-02-16 23:39:19 +0800

    update

--- src/main/java/com/taobao/ibalance/alimama/AddTaoBaoKeEvent.java
+++ src/main/java/com/taobao/ibalance/alimama/AddTaoBaoKeEvent.java
@@ -36,45 +36,42 @@ public class AddTaoBaoKeEvent {
 			}
 		});
 		element.clear();
-		element.sendKeys("guraul");
+		element.sendKeys("");
 
 		element = driver.findElement(By.name("TPL_password"));
 		element.clear();
-		element.sendKeys("raul20161003@tb");
+		element.sendKeys("");
 		ThreadSleep.sleep(1000);
 
 		element = driver.findElement(By.id("J_SubmitStatic"));
 		element.submit();
 
-		ThreadSleep.sleep(3000);
-		driver.navigate().to("http://ad.alimama.com/myunion.htm");
-		ThreadSleep.sleep(3000);
-		driver.navigate().refresh();
-
-		ThreadSleep.sleep(3000);
+		ThreadSleep.sleep(1000);
 		driver.navigate().to("http://ad.alimama.com/myunion.htm#!/promotion/magpie_bridge/active/");
 
-		element = driver.findElement(By.className("message-panel"));
-		element = element.findElement(By.linkText("隐藏"));
+		element = new WebDriverWait(driver, 5).until(new ExpectedCondition<WebElement>() {
+			public WebElement apply(WebDriver d) {
+				return d.findElement(By.className("message-panel")).findElement(By.linkText("隐藏"));
+			}
+		});
 		element.click();
 
 		element = new WebDriverWait(driver, 5).until(new ExpectedCondition<WebElement>() {
 			public WebElement apply(WebDriver d) {
-				return d.findElement(By.id("magix_vf_items_query"));
+				return d.findElement(By.id("magix_vf_items_query")).findElement(By.linkText("15%-20%"));
 			}
 		});
-
-		ThreadSleep.sleep(3000);
-		element = element.findElement(By.linkText("15%-20%"));
 		element.click();
 
 		ThreadSleep.sleep(3000);
 		element = driver.findElement(By.className("table-container"));
 		element.findElement(By.cssSelector(".checkbox.mr8")).click();
 
+		ThreadSleep.sleep(3000);
 		List<WebElement> elements = element.findElements(By.tagName("table"));
 		element = elements.get(1);
 		elements = element.findElements(By.tagName("tr"));
+		String oriWin = driver.getWindowHandle();
 
 		for (WebElement we : elements) {
 			List<WebElement> wes = we.findElements(By.tagName("td"));
@@ -85,15 +82,34 @@ public class AddTaoBaoKeEvent {
 			element = driver.findElement(By.className("dialog-contentbox")).findElement(By.id("vf-dialog"))
 					.findElements(By.tagName("p")).get(1).findElement(By.linkText("知道了"));
 			element.click();
-			
-			element = driver.findElement(By.cssSelector(".normal-form.featured"));
-			element = element.findElement(By.className("featured-cnt"));
-			wes= element.findElements(By.tagName("li"));
-			System.out.println(wes.size());
+
+			ThreadSleep.sleep(3000);
+			gotoNewWin(oriWin, driver);
+			element = driver.findElement(By.id("body")).findElement(By.id("inmain"));
+			element = element.findElement(By.id("vf-main")).findElement(By.xpath("//*[@class='normal-form featured']"));
+
+			// .findElement(By.className("step-wrap"))
+			// .findElement(By.id("vf-step-1")).findElement(By.id("brix_447")).findElement(By.id("featured"))
+			// .findElement(By.className("featured-source"));
+			// element =
+			// element.findElement(By.cssSelector(".normal-form.featured"));
+			// wes= element.findElements(By.tagName("li"));
+			// System.out.println(wes.size());
 			break;
 
 		}
 
+	}
+
+	private static void gotoNewWin(String oriWin, WebDriver driver) {
+		Set<String> handles = driver.getWindowHandles();
+		for (String handle : handles) {
+			if (oriWin.equals(handle)) {
+				continue;
+			} else {
+				driver.switchTo().window(handle);
+			}
+		}
 	}
 
 }
